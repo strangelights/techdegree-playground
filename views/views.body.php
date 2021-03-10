aa50e34d4c3ff161da1f9c6b68846857ab38de70
strangelights <jbrookshire7@gmail.com>
2021-03-10 00:23:58 -0500

    Add repo link

--- views/views.body.php
+++ views/views.body.php
@@ -1,8 +1,12 @@
 <body class="m-4">
     <h1>Hello, world!</h1>
-
+    
     <div class="alert alert-success alert-dismissible fade show" role="alert">
-        This page is built with <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/" class="alert-link" target="_blank">Bootstrap 5!</a>
+        This page is built with <a href="https://getbootstrap.com/docs/5.0/getting-started/introduction/" class="alert-link" target="_blank">Bootstrap 5!</a>&nbsp;
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    <div class="alert alert-info alert-dismissible fade show" role="alert">
+        <a href="https://github.com/strangelights/techdegree-playground" target="_blank" class="alert-link">View on GitHub</a>
         <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
 
@@ -14,5 +18,4 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
-
 </html>
