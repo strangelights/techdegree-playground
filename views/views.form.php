acc43f3e93dbfc4141fe854ae449717eb1ce8500
strangelights <jbrookshire7@gmail.com>
2021-03-11 00:41:50 -0500

    Update button colors

--- views/views.form.php
+++ views/views.form.php
@@ -1,4 +1,4 @@
-<div class="container px-2">    
+<div class="container px-1">    
     <form action="index.php" method="get">
         <div class="form-floating mb-3">
             <input class="form-control" type="number" id="guitarID" name="guitar_id" placeholder="666">
@@ -9,8 +9,8 @@
             <label for="userString">Enter a Amp ID number</label>
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
-        <button type="submit" name="random" value="true" class="btn btn-primary">Random</button>
-        <button type="submit" name="show_all" value="true" class="btn btn-primary">Show All</button>
-        <button type="button" onclick="window.location.href='http://localhost/index.php';" class="btn btn-primary">Clear</button>
+        <button type="submit" name="random" value="true" class="btn btn-success">Random</button>
+        <button type="submit" name="show_all" value="true" class="btn btn-warning">Show All</button>
+        <button type="button" onclick="window.location.href='http://localhost/index.php';" class="btn btn-danger">Clear</button>
     </form>
 </div>
