1fdca5be624181c52034fa8a7991eda79f2c642d
strangelights <jbrookshire7@gmail.com>
2021-04-29 12:40:00 -0400

    Styled pagination links

--- controllers/controllers.pagination.php
+++ controllers/controllers.pagination.php
@@ -1,39 +1,41 @@
 <?php
 
-// Pagination
-
-// TODO Add Bootstrap styling to pagination links
 $items_per_page = 3;
 
 if (isset($_GET["pg"])) {
     $current_page = filter_input(INPUT_GET, "pg", FILTER_SANITIZE_NUMBER_INT);
 }
+
+// Default to page 1
 if (empty($current_page)) {
     $current_page = 1; 
 }
+
 $section = null; // no $sections yet
+
 $total_guitars = get_catalog_count_guitars($section);
 $total_guitar_pages = ceil($total_guitars / $items_per_page);
 
 $total_amps = get_catalog_count_amps($section);
 $total_amp_pages = ceil($total_amps / $items_per_page);
 
-$pagination = "<div class=\"pagination\">";
-$pagination .= "Pages: ";  
+/*
+Pagination logic only checks $total_guitar_pages for now and loops out total pages based on $items_per_page setting at the top of the file 
+*/
+
+$pagination = "<nav aria-label=\"...\"><ul class=\"pagination pagination-sm justify-content-end\">";
 for ($i = 1;$i <= $total_guitar_pages;$i++) {
     if ($i == $current_page) {
-        $pagination .= " <span>$i</span>";
+        $pagination .= "<li class=\"page-item active\" aria-current=\"page\"><span class=\"page-link\">$i</span></li>";
     } else {
-        $pagination .= " <a href='index.php?show_all=true&";
+        $pagination .= "<li class=\"page-item\"><a class=\"page-link\" href='index.php?show_all=true&";
         if (!empty($search)) {
             $pagination .= "s=".urlencode(htmlspecialchars($search))."&";
         } 
-        $pagination .= "pg=$i'>$i</a>";
+        $pagination .= "pg=$i'>$i</a></li>";
     }
 }
-$pagination .= "</div>";
-
-
+$pagination .= "</ul></nav>";
 
 // Limit results in redirect *unused*
 // $limit_results = "";
